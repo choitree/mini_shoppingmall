67c1d5eb65381a87ad78e5838552a93b781f2f77
Tree <gmldbs1109@naver.com>
2022-07-14 14:12:10 +0900

    Repository 생성 [#6]
    - 기본 repository interface 생성

---
+++ src/main/java/com/musinsa/mission/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.musinsa.mission.repository;
+
+import com.musinsa.mission.domain.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}

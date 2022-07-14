c709e317e1340ef0b599a22bc00751e0d18fb560
Tree <gmldbs1109@naver.com>
2022-07-15 03:51:24 +0900

    각 카테고리별 최저값 추려서 리스트 및 총액 반환
    - 가격과 카테고리가 동일한 제품의 중복 제거를 위한 equals, hashCode 메소드 재정의

--- src/main/java/com/musinsa/mission/domain/Item.java
+++ src/main/java/com/musinsa/mission/domain/Item.java
@@ -3,6 +3,7 @@ package com.musinsa.mission.domain;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -25,4 +26,17 @@ public class Item {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(price, item.price) && Objects.equals(category, item.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, category);
+    }
 }

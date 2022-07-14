6848e7dd750dabd808c2e5da04fc83b8dcf5c43c
Tree <gmldbs1109@naver.com>
2022-07-15 03:50:14 +0900

    각 카테고리별 최저값 추려서 리스트 및 총액 반환
    - service단에 메소드 추가
    - repository에서는 동일한 가격의 제품이 2개 찾아져서 서비스단에서 중복 제거

--- src/main/java/com/musinsa/mission/service/item/ItemService.java
+++ src/main/java/com/musinsa/mission/service/item/ItemService.java
@@ -1,10 +1,13 @@
 package com.musinsa.mission.service.item;
 
-import com.musinsa.mission.dto.item.ItemResponseDTO;
+import com.musinsa.mission.dto.item.ItemSimpleResponseDTO;
+import com.musinsa.mission.dto.item.ItemsResponseByEachCategoryMinPriceDTO;
 
 import java.util.List;
 
 public interface ItemService {
 
-    List<ItemResponseDTO> findByCategoryLowestAndHighest(String categoryName);
+    ItemsResponseByEachCategoryMinPriceDTO findAllByEachCategoryMinPrice();
+
+    List<ItemSimpleResponseDTO> findByCategoryLowestAndHighest(String categoryName);
 }
--- src/main/java/com/musinsa/mission/service/item/ItemServiceImpl.java
+++ src/main/java/com/musinsa/mission/service/item/ItemServiceImpl.java
@@ -3,6 +3,8 @@ package com.musinsa.mission.service.item;
 import com.musinsa.mission.domain.Category;
 import com.musinsa.mission.domain.Item;
 import com.musinsa.mission.dto.item.ItemResponseDTO;
+import com.musinsa.mission.dto.item.ItemSimpleResponseDTO;
+import com.musinsa.mission.dto.item.ItemsResponseByEachCategoryMinPriceDTO;
 import com.musinsa.mission.exception.CategoryNotFoundException;
 import com.musinsa.mission.exception.config.ErrorCode;
 import com.musinsa.mission.repository.CategoryRepository;
@@ -21,13 +23,24 @@ public class ItemServiceImpl implements ItemService {
     private final CategoryRepository categoryRepository;
 
     @Override
-    public List<ItemResponseDTO> findByCategoryLowestAndHighest(String categoryName) {
+    public ItemsResponseByEachCategoryMinPriceDTO findAllByEachCategoryMinPrice() {
+        List<Item> items = itemRepository.findAllByEachCategoryMinPrice();
+        items = items.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        List<ItemResponseDTO> itemResponseDTOS = items.stream()
+                .map(item -> ItemResponseDTO.from(item))
+                .collect(Collectors.toList());
+        return ItemsResponseByEachCategoryMinPriceDTO.from(itemResponseDTOS);
+    }
+
+    @Override
+    public List<ItemSimpleResponseDTO> findByCategoryLowestAndHighest(String categoryName) {
         Category category = categoryRepository.findByName(categoryName)
                 .orElseThrow(() -> new CategoryNotFoundException(ErrorCode.CATEGORY_NOT_FOUND, "조회하려는 카테고리가 존재하지 않습니다."));
         List<Item> items = itemRepository.findByCategoryLowestAndHighest(category);
         return items.stream()
-                .map(item -> ItemResponseDTO.from(item))
+                .map(item -> ItemSimpleResponseDTO.from(item))
                 .collect(Collectors.toList());
     }
-
 }

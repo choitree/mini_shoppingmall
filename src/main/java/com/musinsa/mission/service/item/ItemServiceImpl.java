3712b91f6d61f2108e07f7f11a53bc482ff319ed
Tree <gmldbs1109@naver.com>
2022-07-14 16:07:43 +0900

    변경된 Exception 호출 [#7]
    - service에서 변경된 Exception 호출되도록 인자 변경

--- src/main/java/com/musinsa/mission/service/item/ItemServiceImpl.java
+++ src/main/java/com/musinsa/mission/service/item/ItemServiceImpl.java
@@ -4,6 +4,7 @@ import com.musinsa.mission.domain.Category;
 import com.musinsa.mission.domain.Item;
 import com.musinsa.mission.dto.item.ItemResponseDTO;
 import com.musinsa.mission.exception.CategoryNotFoundException;
+import com.musinsa.mission.exception.config.ErrorCode;
 import com.musinsa.mission.repository.CategoryRepository;
 import com.musinsa.mission.repository.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +22,8 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<ItemResponseDTO> findByCategoryLowestAndHighest(String categoryName) {
-        Category category = categoryRepository.findByName(categoryName).orElseThrow(() -> new CategoryNotFoundException());
+        Category category = categoryRepository.findByName(categoryName)
+                .orElseThrow(() -> new CategoryNotFoundException(ErrorCode.CATEGORY_NOT_FOUND, "조회하려는 카테고리가 존재하지 않습니다."));
         List<Item> items = itemRepository.findByCategoryLowestAndHighest(category);
         return items.stream()
                 .map(item -> ItemResponseDTO.from(item))

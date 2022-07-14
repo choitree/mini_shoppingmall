18d007678b06e9aed8b571b66f7c96c042ab4203
Tree <gmldbs1109@naver.com>
2022-07-14 18:33:46 +0900

    카테고리별 최소값과 제품 브랜드를 구하는 쿼리 작성
    - from절에서는 서브 쿼리 사용 불가능해서 작동하지는 않는 쿼리

--- src/main/java/com/musinsa/mission/repository/item/ItemRepositoryImpl.java
+++ src/main/java/com/musinsa/mission/repository/item/ItemRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.musinsa.mission.repository.item;
 
 import com.musinsa.mission.domain.Category;
 import com.musinsa.mission.domain.Item;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +29,32 @@ public class ItemRepositoryImpl implements ItemRepositoryCustom {
                                                 .where(item.category.eq(category))
                                 )))
                         .and(item.category.eq(category)))
+                .fetch();
+    }
+
+    /**
+     *
+     * 카테고리별 최소값과 제품의 브랜드를 추출
+     * querydsl은 from 절에 서브쿼리 사용 불가능
+    select i.category_id, i.brand_id, i.price
+    from(
+        select category_id, min(price) as minprice
+        from item
+        group by category_id
+    ) as j
+    inner join item as i on i.category_id = j.category_id and i.price = j.minprice;
+     */
+
+    public List<Item> findAllByEachCategoryMinPrice() {
+        List<Tuple> results =  queryFactory
+                .select(item.category, item.brand, item.price)
+                .from(queryFactory
+                        .select(item.category, item.price.min())
+                        .from(item)
+                        .groupBy(item.category))
+                .innerJoin(item.as("i1"), item.as("i2"))
+                .on(item.category.eq("i2.category")
+                        .and(item.price).eq("i2.minprice"))
                 .fetch();
     }
 }

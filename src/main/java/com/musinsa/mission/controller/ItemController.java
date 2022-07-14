8d64157a4413e71bf85822f7548dd0f39cbc4048
Tree <gmldbs1109@naver.com>
2022-07-15 03:37:55 +0900

    각 카테고리별 최저값 추려서 리스트 및 총액 반환
    - controller에 메소드 생성

--- src/main/java/com/musinsa/mission/controller/ItemController.java
+++ src/main/java/com/musinsa/mission/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.musinsa.mission.controller;
 
-import com.musinsa.mission.dto.item.ItemResponseDTO;
+import com.musinsa.mission.dto.item.ItemSimpleResponseDTO;
+import com.musinsa.mission.dto.item.ItemsResponseByEachCategoryMinPriceDTO;
 import com.musinsa.mission.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -22,8 +23,15 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @GetMapping("/minCase")
+    public ResponseEntity<ItemsResponseByEachCategoryMinPriceDTO> findAllByEachCategoryMinPrice() {
+        logger.info("각 카테고리에서 최저값으로 추려서 리스트로 조회");
+        return ResponseEntity.ok(itemService.findAllByEachCategoryMinPrice());
+    }
+
+
     @GetMapping("/category")
-    public ResponseEntity<List<ItemResponseDTO>> findByCategoryLowestAndHighest(@RequestParam String name) {
+    public ResponseEntity<List<ItemSimpleResponseDTO>> findByCategoryLowestAndHighest(@RequestParam String name) {
         logger.info("카테고리별 최소, 최대값 조회");
         return ResponseEntity.ok(itemService.findByCategoryLowestAndHighest(name));
     }

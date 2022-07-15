73f20a33e27c4070426a33fc3cfe36c2d4b45a6b
Tree <gmldbs1109@naver.com>
2022-07-15 16:10:26 +0900

    entity에 정의한 equals, hashCode 메소드 삭제

--- src/main/java/com/musinsa/mission/domain/Item.java
+++ src/main/java/com/musinsa/mission/domain/Item.java
@@ -3,7 +3,6 @@ package com.musinsa.mission.domain;
 import lombok.Getter;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Getter
 @Entity
@@ -26,17 +25,4 @@ public class Item {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return Objects.equals(price, item.price) && Objects.equals(category, item.category);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(price, category);
-    }
 }

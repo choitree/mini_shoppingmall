b999d367eec7b8b10d09d0d346f4cf49d619aa93
Tree <gmldbs1109@naver.com>
2022-07-15 16:09:46 +0900

    @JsonProperty 추가, 클래스명 변경

--- src/main/java/com/musinsa/mission/dto/item/ItemsByEachCategoryMinPriceResponseDTO.java
+++ src/main/java/com/musinsa/mission/dto/item/ItemsByEachCategoryMinPriceResponseDTO.java
@@ -1,26 +1,29 @@
 package com.musinsa.mission.dto.item;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@Builder
 @Getter
 @RequiredArgsConstructor
-@Builder
-public class ItemsResponseByEachCategoryMinPriceDTO {
+public class ItemsByEachCategoryMinPriceResponseDTO {
 
+    @JsonProperty("카테고리별 최저가 아이템")
     private final List<ItemResponseDTO> itemResponseDTOS;
 
+    @JsonProperty("총합")
     private final Integer totalPrice;
 
-    public static ItemsResponseByEachCategoryMinPriceDTO from(List<ItemResponseDTO> itemResponseDTOS) {
+    public static ItemsByEachCategoryMinPriceResponseDTO from(List<ItemResponseDTO> itemResponseDTOS) {
         Integer totalPrice = itemResponseDTOS.stream()
                 .mapToInt(itemResponseDTO -> itemResponseDTO.getItemPrice())
                 .sum();
 
-        return ItemsResponseByEachCategoryMinPriceDTO.builder()
+        return ItemsByEachCategoryMinPriceResponseDTO.builder()
                 .itemResponseDTOS(itemResponseDTOS)
                 .totalPrice(totalPrice)
                 .build();

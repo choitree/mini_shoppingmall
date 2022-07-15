fa1b78f96743a001cba84e0919ecb355aff3edaa
Tree <gmldbs1109@naver.com>
2022-07-15 16:06:46 +0900

    카테고리별 최저/최고가 조회하는 dto 생성
    - 값은 list로 조회하고, dto에서 변환해서 response하는 방식

---
+++ src/main/java/com/musinsa/mission/dto/item/ItemsMinAndMixByCategoryResponseDTO.java
@@ -0,0 +1,28 @@
+package com.musinsa.mission.dto.item;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class ItemsMinAndMixByCategoryResponseDTO {
+
+    @JsonProperty("최소")
+    private final ItemSimpleResponseDTO minItem;
+
+    @JsonProperty("최대")
+    private final ItemSimpleResponseDTO maxItem;
+
+    public static ItemsMinAndMixByCategoryResponseDTO convertListToItemsMinAndMixByCategoryResponseDTO(List<ItemSimpleResponseDTO> itemSimpleResponseDTOS) {
+        return ItemsMinAndMixByCategoryResponseDTO.builder()
+                .minItem(itemSimpleResponseDTOS.get(0))
+                .maxItem(itemSimpleResponseDTOS.get(1))
+                .build();
+    }
+}

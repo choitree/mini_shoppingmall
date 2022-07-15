d1fded1d6c1a4f3dce55773a1a35805a8315a98d
Tree <gmldbs1109@naver.com>
2022-07-15 16:09:09 +0900

    @JsonProperty 추가, 사용하지 않는 메소드 삭제

--- src/main/java/com/musinsa/mission/dto/item/ItemSimpleResponseDTO.java
+++ src/main/java/com/musinsa/mission/dto/item/ItemSimpleResponseDTO.java
@@ -1,25 +1,19 @@
 package com.musinsa.mission.dto.item;
 
-import com.musinsa.mission.domain.Item;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemSimpleResponseDTO {
 
+    @JsonProperty("브랜드")
     private String brandName;
-    private Integer itemPrice;
 
-    public static ItemSimpleResponseDTO from(Item item) {
-        return ItemSimpleResponseDTO.builder()
-                .brandName(item.getBrand().getName())
-                .itemPrice(item.getPrice())
-                .build();
-    }
+    @JsonProperty("금액")
+    private Integer itemPrice;
 }
 

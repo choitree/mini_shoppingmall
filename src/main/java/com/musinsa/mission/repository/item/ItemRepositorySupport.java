bb80df0f4fe3b4fe74b0181485305462bc259e73
Tree <gmldbs1109@naver.com>
2022-07-15 15:50:25 +0900

    Querydsl에 사용되는 repository 변경
    - RepositorySupport로 변경 및 기존 클래스/인터페이스 삭제
    - 조회 구문에서 모두 DTO로 리턴되도록 메소드 수정(성능 목적)

--- src/main/java/com/musinsa/mission/repository/item/ItemRepositorySupport.java
+++ src/main/java/com/musinsa/mission/repository/item/ItemRepositorySupport.java
@@ -5,58 +5,75 @@ import com.musinsa.mission.domain.Item;
 import com.musinsa.mission.domain.QItem;
 import com.musinsa.mission.dto.item.ItemResponseDTO;
 import com.musinsa.mission.dto.item.ItemSimpleResponseDTO;
+import com.querydsl.core.dml.DeleteClause;
+import com.querydsl.core.dml.UpdateClause;
+import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPADeleteClause;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import org.hibernate.Hibernate;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static com.musinsa.mission.domain.QItem.item;
 
-@RequiredArgsConstructor
-public class ItemRepositoryImpl implements ItemRepositoryCustom {
+@Repository
+public class ItemRepositorySupport extends QuerydslRepositorySupport {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Item> findAllByEachCategoryMinPrice() {
+    public ItemRepositorySupport(JPAQueryFactory queryFactory) {
+        super(Item.class);
+        this.queryFactory = queryFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ItemResponseDTO> findAllByEachCategoryMinPrice() {
         QItem item1 = new QItem("item1");
 
-        List<Item> itemResponseDTOS = queryFactory
-                .selectFrom(item)
+        List<ItemResponseDTO> itemResponseDTOS = queryFactory
+                .select(Projections.constructor(ItemResponseDTO.class,
+                        item.category.name.as("categoryName"),
+                        item.brand.name.as("brandName"),
+                        item.price.as("itemPrice")
+                ))
+                .from(item)
                 .where(item.price.eq(
                         JPAExpressions
                                 .select(item1.price.min())
                                 .from(item1)
                                 .where(item1.category.name.eq(item.category.name))
                 ))
-                .orderBy(item.category.name.asc())
+                .orderBy(item.category.id.asc())
                 .fetch();
         return itemResponseDTOS;
     }
 
-    /*
-    select sum(price), brand_id
-    from item
-    group by brand_id
-    order by sum(price) asc limit 1;
-     */
+    @Transactional(readOnly = true)
     public ItemSimpleResponseDTO findCheapestBrandSumOfAllCategory() {
         return queryFactory
                 .from(item)
                 .select(Projections.constructor(ItemSimpleResponseDTO.class,
                         item.brand.name.as("brandName"),
                         item.price.sum().as("itemPrice")
-
                 ))
                 .groupBy(item.brand.name)
                 .orderBy(item.price.sum().asc())
                 .fetchFirst();
     }
 
-    public List<Item> findByCategoryLowestAndHighest(Category category) {
-        List<Item> items = queryFactory.selectFrom(item)
+    @Transactional(readOnly = true)
+    public List<ItemSimpleResponseDTO> findByCategoryLowestAndHighest(Category category) {
+        return queryFactory.select(Projections.constructor(ItemSimpleResponseDTO.class,
+                        item.brand.name,
+                        item.price
+                ))
+                .from(item)
                 .where((item.price.eq(
                                 JPAExpressions
                                         .select(item.price.min())
@@ -71,8 +88,7 @@ public class ItemRepositoryImpl implements ItemRepositoryCustom {
                                                 .where(item.category.eq(category))
                                 )))
                         .and(item.category.eq(category)))
+                .orderBy(item.price.asc())
                 .fetch();
-        items.stream().map(Item::getBrand).forEach(Hibernate::initialize);
-        return items;
     }
 }

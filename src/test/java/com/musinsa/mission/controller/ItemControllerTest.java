b467e6d070be03dbad224dc3f45cc272a4db33f1
Tree <gmldbs1109@naver.com>
2022-07-16 16:13:26 +0900

    ItemControllerTest 내 테스트 코드 추가

---
+++ src/test/java/com/musinsa/mission/controller/ItemControllerTest.java
@@ -0,0 +1,94 @@
+package com.musinsa.mission.controller;
+
+import com.musinsa.mission.dto.item.response.ItemResponseDTO;
+import com.musinsa.mission.dto.item.response.ItemSimpleResponseDTO;
+import com.musinsa.mission.dto.item.response.ItemsByEachCategoryMinPriceResponseDTO;
+import com.musinsa.mission.dto.item.response.ItemsMinAndMixByCategoryResponseDTO;
+import com.musinsa.mission.service.item.ItemServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ItemServiceImpl itemService;
+
+    @Test
+    @DisplayName("카테고리별 최저가 뽑아서 리스트, 총액 검사")
+    void findAllByEachCategoryMinPrice() throws Exception {
+        //given
+        ItemsByEachCategoryMinPriceResponseDTO result = ItemsByEachCategoryMinPriceResponseDTO.from(
+                List.of(
+                        new ItemResponseDTO("상의", "C", 10000),
+                        new ItemResponseDTO("아우터", "E", 5000),
+                        new ItemResponseDTO("액세서리", "F", 1900)
+                )
+        );
+
+        given(itemService.findAllByEachCategoryMinPrice())
+                .willReturn(result);
+
+        mockMvc.perform(get("/item/minCase"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.최저가리스트.[0].카테고리").value(equalTo("상의")))
+                .andExpect(jsonPath("$.최저가리스트.[0].브랜드").value(equalTo("C")))
+                .andExpect(jsonPath("$.최저가리스트.[0].가격").value(equalTo(10000)))
+                .andExpect(jsonPath("총합").value(equalTo(16900)));
+
+    }
+
+    @Test
+    @DisplayName("한 브랜드의 합산액이 최저가인 브랜드 및 금액 테스트")
+    void findCheapestBrandSumOfAllCategory() throws Exception {
+
+        ItemSimpleResponseDTO result = new ItemSimpleResponseDTO("A", 55000);
+
+        //given
+        given(itemService.findCheapestBrandSumOfAllCategory())
+                .willReturn(result);
+
+        mockMvc.perform(get("/item/cheapestBrand"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("브랜드").value(equalTo("A")))
+                .andExpect(jsonPath("금액").value(equalTo(55000)));
+    }
+
+    @Test
+    @DisplayName("카테고리별 최소/최대 금액, 브랜드 테스트")
+    void findByCategoryLowestAndHighest() throws Exception {
+
+        ItemsMinAndMixByCategoryResponseDTO result = ItemsMinAndMixByCategoryResponseDTO
+                        .convertListToItemsMinAndMixByCategoryResponseDTO(List.of(
+                                new ItemSimpleResponseDTO("A", 100),
+                                new ItemSimpleResponseDTO("B", 500)
+                        ));
+
+        given(itemService.findByCategoryLowestAndHighest("top"))
+                .willReturn(result);
+
+        mockMvc.perform(get("/item/category")
+                .param( "name", "top"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.최소.브랜드").value(equalTo("A")))
+                .andExpect(jsonPath("$.최소.금액").value(equalTo(100)))
+                .andExpect(jsonPath("$.최대.브랜드").value(equalTo("B")))
+                .andExpect(jsonPath("$.최대.금액").value(equalTo(500)));
+    }
+
+}

424b0a65500b54ac302f0f5f648ded5b4b27c945
Tree <gmldbs1109@naver.com>
2022-07-17 10:42:41 +0900

    Builder 추가

--- src/main/java/com/musinsa/mission/dto/item/response/ItemResponseDTO.java
+++ src/main/java/com/musinsa/mission/dto/item/response/ItemResponseDTO.java
@@ -1,7 +1,10 @@
 package com.musinsa.mission.dto.item.response;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.musinsa.mission.domain.Item;
 import lombok.*;
 
 @Getter
@@ -9,7 +12,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(exclude = {"brandName"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ItemResponseDTO {
+
+    @JsonIgnore
+    private Long itemId;
 
     @JsonProperty("카테고리")
     private String categoryName;
@@ -19,4 +26,20 @@ public class ItemResponseDTO {
 
     @JsonProperty("가격")
     private Integer itemPrice;
+
+    private String itemName;
+
+    public ItemResponseDTO(String categoryName, String brandName, Integer itemPrice) {
+        this.categoryName = categoryName;
+        this.brandName = brandName;
+        this.itemPrice = itemPrice;
+    }
+
+    public static ItemResponseDTO entityToDto(Item item) {
+        return ItemResponseDTO.builder()
+                .categoryName(item.getCategory().getName())
+                .brandName(item.getBrand().getName())
+                .itemPrice(item.getPrice())
+                .build();
+    }
 }
